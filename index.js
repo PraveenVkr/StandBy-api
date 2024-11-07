9fd5453e8c65bba91f11c98f8d45c406e73b6d61
Praveen <praveenvkr21@gmail.com>
2024-11-07 17:05:16 +0530

    changes cors

--- index.js
+++ index.js
@@ -17,12 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
